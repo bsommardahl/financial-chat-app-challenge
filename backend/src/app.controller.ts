cd3c01a72271c8f41c78f3645e6497b4a3e0cdb5
Renán Zelaya <renan_zelaya@hotmail.com>
2020-10-16 00:13:14 -0600

    Add backend implementation to send messages using rabbitmq

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject('MESSAGE_SERVICE') private readonly client: ClientProxy,
+  ) {}
+
+  async onApplicationBootstrap() {
+    await this.client.connect();
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Query('message') message: string): string {
+    this.client.emit<unknown>('some-message', { message });
+    return `${message} printed`;
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomModule } from './modules/room/room.module';
 
 @Module({
-  imports: [RoomModule],
+  imports: [
+    RoomModule,
+    ClientsModule.register([
+      {
+        name: 'MESSAGE_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://rabbitmq:5672/messages'],
+          queue: 'messages_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
