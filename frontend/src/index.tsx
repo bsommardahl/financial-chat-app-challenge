b1a97a4cc7ce70662a25ae3d0d6fa40dac209653
Renán Zelaya <renan_zelaya@hotmail.com>
2020-10-21 15:47:10 -0600

    Removed unused react-redux store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import store from "./store";
 import {
   FirebaseContext,
   FirebaseProvider,
@@ -12,13 +10,11 @@ import {
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <FirebaseProvider>
-        <FirebaseContext.Consumer>
-          {(props) => !props?.loading && <App />}
-        </FirebaseContext.Consumer>
-      </FirebaseProvider>
-    </Provider>
+    <FirebaseProvider>
+      <FirebaseContext.Consumer>
+        {(props) => !props?.loading && <App />}
+      </FirebaseContext.Consumer>
+    </FirebaseProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
